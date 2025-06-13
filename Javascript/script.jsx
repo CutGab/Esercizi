e03ca781abd1522159c98452f7e9cd06a923140d
CutGab <gabriele.cutolo2003@gmail.com>
2025-06-13 09:07:47 +0200

    Fatte cose

---
+++ Javascript/script.jsx
@@ -0,0 +1,32 @@
+const rootElement = document.querySelector("#root");
+
+const root = ReactDOM.createRoot(rootElement);
+
+const App = (props) => {
+    return (
+        <main className="main">
+            <h1> Primo componente </h1>
+             {props.children}
+        </main>
+        
+    )
+}
+
+const List = () => {
+    return(
+        <ul>
+            <li> JAVASCRIPT</li>
+            <li> PHP </li>
+            <li> PYTHON </li>
+        </ul>
+    )
+}
+
+root.render(
+    <>
+        <App>
+            <h2> Lista dev </h2>
+            <List></List>   
+        </App>    
+    </>
+)
---
+++ Python/Azienda/impiegato.py
@@ -0,0 +1,46 @@
+from progetto import Progetto
+from coinvolto import _coinvolto
+from custom_types import RealGEZ
+
+from datetime import date
+
+class Impiegato:
+    _nome: str # noto alla nascita
+    _cognome: str # noto alla nascita
+    _nascita: date # <<immutable>>, # noto alla nascita
+    _stipendio: RealGEZ # # noto alla nascita
+
+    _progetti: dict[Progetto, _coinvolto]
+
+    def __init__(self, nome:str, cognome:str, nascita: date, stipendio: RealGEZ) -> None:
+        self.set_nome(nome)
+        self.set_cognome(cognome)
+        self._nascita = nascita
+        self.set_stipendio(stipendio)
+        self._progetti = dict()
+
+    def nome(self) -> str:
+        return self._nome
+
+    def cognome(self) -> str:
+        return self._cognome
+
+    def nascita(self) -> date:
+        return self._nascita
+
+    def stipendio(self) -> RealGEZ:
+        return self._stipendio
+
+    def set_nome(self, nome: str) -> None:
+        self._nome = nome
+
+    def set_cognome(self, cognome: str) -> None:
+        self._cognome = cognome
+
+    def set_stipendio(self, stipendio: RealGEZ) -> None:
+        self._stipendio = stipendio
+
+    def __str__(self) -> str:
+        return (f"{self.nome()} {self.cognome()}, "
+                f"nascita: {self.nascita()}, "
+                f"stipendio: {self.stipendio()}")
